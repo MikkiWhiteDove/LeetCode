56315195f8559e4a15192f79ed11c5c496668e5c
mikki <mishanabelov2015@icloud.com>
2023-06-15 20:46:02 +0300

    LeetCode

---
+++ LeetCode/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by Mishana on 15.06.2023.
+//
+import Foundation
+
+// 1920. Build Array from Permutation -- Easy [Arrays]
+
+//class Solution {
+//    func buildArray(_ nums: [Int]) -> [Int] {
+//        var ans:[Int] = []
+//        for i in 0..<nums.count {
+//            ans.append(nums[nums[i]])
+//        }
+//        print(ans)
+//        return ans
+//    }
+//}
+//Solution().buildArray([0,2,1,5,3,4])
+
